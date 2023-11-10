9d2d281c0101ce5ba0ac3bf77632bf308f42f45f
Martin Barros <martinbarros86@gmail.com>
2023-11-10 18:29:28 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+require("dotenv/config")
+app.use(express.static('public'));
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, ()=>{
+    console.log('Servidor funcionando en puerto: ' + PORT);
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.get('/position', (req,res)=>{
+    res.sendFile(__dirname + '/views/position.html');
+});
